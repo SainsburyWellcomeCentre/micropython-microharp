08fcfa5b042ca205662137b05622c84952839bcf
DCisHurt <roger33669@gmail.com>
2026-04-16 13:06:09 +0100

    add __init__.py for microharp module; remove unused assets and firmware binary

---
+++ __init__.py
@@ -0,0 +1,31 @@
+from .device import HarpDevice
+from .message import HarpMessage
+
+
+_attrs = {
+    "HarpClock": "clock",
+    "HarpDevice": "device",
+    "HarpEvent": "event",
+    "PinEvent": "event",
+    "PeriodicEvent": "event",
+    "LooseEvent": "event",
+    "HarpMessage": "message",
+    "HarpRxMessage": "message",
+    "HarpTxMessage": "message",
+    "HarpRegister": "register",
+    "ReadWriteReg": "register",
+    "ReadOnlyReg": "register",
+    "OperationalCtrlReg": "register",
+    "TimestampSecondReg": "register",
+    "TimestampMicroReg": "register",
+    "PinRegister": "register",
+    "HarpTypes": "type",
+}
+
+
+def __getattr__(attr):
+    if attr in _attrs:
+        module_name = _attrs[attr]
+        module = __import__(f"microharp.{module_name}", fromlist=[attr])
+        return getattr(module, attr)
+    raise AttributeError(f"module 'microharp' has no attribute '{attr}'")
